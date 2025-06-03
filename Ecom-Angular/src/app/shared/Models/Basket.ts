f537f77f801b417cde25b001c8bf41674715a9a5
mostafa.afifi <mostafa.afifi@SW-MOSTAFA>
2025-06-03 18:15:37 +0300

    Add Basket On Angular

---
+++ Ecom-Angular/src/app/shared/Models/Basket.ts
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IBasket {
+  id: string;
+  basketItems: IBasketItem[];
+}
+
+export interface IBasketItem {
+  id: string;
+  name: string;
+  quantity: number;
+  imageURL: string;
+  price: number;
+  category: string;
+}
+export class Basket implements IBasket {
+  id = uuidv4();
+  basketItems: IBasketItem[];
+}
