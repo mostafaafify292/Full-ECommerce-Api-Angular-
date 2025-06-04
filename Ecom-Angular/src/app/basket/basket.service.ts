cc2f223df6b9d946f38f9d7bb12b00cbef125193
mostafa.afifi <mostafa.afifi@SW-MOSTAFA>
2025-06-04 18:14:17 +0300

    basket service

--- Ecom-Angular/src/app/basket/basket.service.ts
+++ Ecom-Angular/src/app/basket/basket.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { IBasket } from '../shared/Models/Basket';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BasketService {
+  constructor(private http: HttpClient) {}
+  baseURL = 'https://localhost:44330/api/';
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket = this.basketSource.asObservable();
 
-  constructor() { }
+  GetBasket(id: string) {
+    return this.http.get(this.baseURL + 'Basket/get-basket-item/' + id).pipe(
+      map((value: IBasket) => {
+        this.basketSource.next(value);
+      }),
+    );
+  }
+
+  SetBasket(basket: IBasket) {
+    return this.http
+      .post(this.baseURL + 'Basket/update-basket/', basket)
+      .subscribe({
+        next: (value: IBasket) => {
+          this.basketSource.next(value);
+        },
+        error(err) {
+          console.log(err);
+        },
+      });
+  }
+
+  GetCurrentValue() {
+    return this.basketSource.value;
+  }
 }
